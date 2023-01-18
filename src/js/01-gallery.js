0e427c85367882ed157daddc179a2f46c9d2be16
Sergei Markov <mar11k1985855@gmail.com>
2023-01-18 13:41:13 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMurkup = createGalleryMurkup(galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMurkup);
+
+function createGalleryMurkup(Items) {
+  return Items.map(({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a> </li>`;
+  }).join(' ');
+}
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.feedback-form');
+const formTextarea = document.querySelector('.feedback-form textarea');
+const formEmailInput = document.querySelector('.feedback-form input');
 
+// form.addEventListener('submit');
+formTextarea.addEventListener('input', onTextareaInput);
+// formEmailInput.addEventListener('input');
+form.addEventListener('input', e => {
+  const {
+    elements: { email, textarea },
+  } = e.target;
+  console.log(email);
+});
+function onTextareaInput(e) {
+  const message = e.target.value;
+  console.log(message);
+  localStorage.setItem('feedback-msg', message);
+}
