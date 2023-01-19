25c2b836d09f0bd777060a0dcdc78d54585d8cff
Sergei Markov <mar11k1985855@gmail.com>
2023-01-19 13:23:59 +0200

    final js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_PLAYER_TIME = 'videoplayer-current-time';
+const onPlay = ({ seconds }) => {
+  localStorage.setItem(CURRENT_PLAYER_TIME, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(CURRENT_PLAYER_TIME));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,18 +1,37 @@
+import throttle from 'lodash.throttle';
+
 const form = document.querySelector('.feedback-form');
 const formTextarea = document.querySelector('.feedback-form textarea');
 const formEmailInput = document.querySelector('.feedback-form input');
+const FEEDBACK_DATA = 'feedback-form-state';
 
-// form.addEventListener('submit');
-formTextarea.addEventListener('input', onTextareaInput);
-// formEmailInput.addEventListener('input');
-form.addEventListener('input', e => {
-  const {
-    elements: { email, textarea },
-  } = e.target;
-  console.log(email);
-});
-function onTextareaInput(e) {
-  const message = e.target.value;
-  console.log(message);
-  localStorage.setItem('feedback-msg', message);
+const feedbackObject = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  feedbackObject[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK_DATA, JSON.stringify(feedbackObject));
 }
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_DATA);
+  console.log(feedbackObject);
+}
+
+function fillInput() {
+  const savedData = localStorage.getItem(FEEDBACK_DATA);
+  const parsedData = JSON.parse(savedData);
+  if (savedData) {
+    console.log(parsedData);
+    formEmailInput.value = parsedData.email;
+    formTextarea.value = parsedData.message;
+  }
+}
+fillInput();
